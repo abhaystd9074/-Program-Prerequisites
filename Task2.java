c6aea41e406c51178f76785649dadbdb43694a4f
abhaystd9074 <abhaygautam907457@gmail.com>
2025-01-08 12:11:20 +0530

    task2 is added

---
+++ Task2.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class Task2{
+public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter first number = " );
+ int f=sc.nextInt();
+ System.out.println("enter second number = " );
+ int s=sc.nextInt();
+ System.out.println("result of addition = "  + (f+s));
+}
+
+}
