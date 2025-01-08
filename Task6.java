6aa94f95f2f0bf07353c56acd57ee79539329bbd
abhaystd9074 <abhaygautam907457@gmail.com>
2025-01-08 16:47:42 +0530

    task6 is added

---
+++ Task6.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Task6{
+public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter principal = " );
+ 
+ int p =sc.nextInt();
+ System.out.println("enter rate = " );
+ 
+ int r =sc.nextInt();
+ System.out.println("enter time = " );
+ 
+ int t =sc.nextInt();
+ 
+ 
+ System.out.println(" S.I will be   = "  + ((p*r*t)/100));
+}
+
+}
