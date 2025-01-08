775d3519a374895fe2cbc72d2b07c7eafd501d5c
abhaystd9074 <abhaygautam907457@gmail.com>
2025-01-08 12:54:13 +0530

    task4 is added

---
+++ Task4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Task4{
+public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter radius of circle = " );
+ 
+ double rad =sc.nextDouble();
+ double pi= ((22*1.0)/7);
+   double res=(pi*1.0*rad*rad);
+ System.out.println("Area of Circle is  = "  + (res));
+}
+
+}
