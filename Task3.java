14b0ff2d2852b8fb5a32e2f752f0df9c5570f6a2
abhaystd9074 <abhaygautam907457@gmail.com>
2025-01-08 12:53:11 +0530

    task3 is added

---
+++ Task3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Task3{
+public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter temperature in celsius = " );
+ double cel=sc.nextDouble();
+   double tem=(cel *(9/5))+32;
+ System.out.println("Temperature in fahrenheit is = "  + (tem));
+}
+
+}
