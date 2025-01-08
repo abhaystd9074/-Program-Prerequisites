283dd6d6e71b0afeca0d35bed74b763c64e3ead3
abhaystd9074 <abhaygautam907457@gmail.com>
2025-01-08 16:50:12 +0530

    task9 is added

---
+++ Task9.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Task9{
+public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter first number = " );
+ int a =sc.nextInt();
+ 
+ System.out.println("enter second number = " );
+ int b =sc.nextInt();
+ 
+ System.out.println("enter third number = " );
+ int c =sc.nextInt();
+ 
+ 
+ System.out.println(" average of three numbers will be   = "  + ((a+b+c)/3));
+}
+
+}
