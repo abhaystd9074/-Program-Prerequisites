1c39cf86075fdf9ebcc7b9950754208135c7678e
abhaystd9074 <abhaygautam907457@gmail.com>
2025-01-08 16:49:20 +0530

    task8 is added

---
+++ Task8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Task8{
+public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter base = " );
+ 
+ int l =sc.nextInt();
+ System.out.println("enter exponent = " );
+ 
+ int w =sc.nextInt();
+ 
+ 
+ System.out.println(" Perimeter of reactangle will be  = "  + (Math.pow(l,w)));
+}
+
+}
