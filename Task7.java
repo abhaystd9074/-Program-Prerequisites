772be6ce37cdfac15d70630d8ce149e24d058446
abhaystd9074 <abhaygautam907457@gmail.com>
2025-01-08 16:48:33 +0530

    task7 is added

---
+++ Task7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Task7{
+public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter length = " );
+ 
+ int l =sc.nextInt();
+ System.out.println("enter width = " );
+ 
+ int w =sc.nextInt();
+ 
+ 
+ System.out.println(" Perimeter of reactangle will be  = "  + (2*(l+w)));
+}
+
+}
