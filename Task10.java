a26091fa072294194f0982f7164b13f921135d24
abhaystd9074 <abhaygautam907457@gmail.com>
2025-01-08 16:50:39 +0530

    task10 is added

---
+++ Task10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Task10{
+public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter Km = " );
+ int a =sc.nextInt();
+ 
+ System.out.println(" conversion of km into miles is = "  + (a*0.621371));
+}
+
+}
